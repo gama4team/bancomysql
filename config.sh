20854a7327056e9d491e3c26728e2e1b02088739
willian <willianr.demelo@gmail.com>
2021-10-27 20:59:11 +0000

    adicionando teste

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 cd 1-ansible
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /root/.ssh/id_rsa
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
 
